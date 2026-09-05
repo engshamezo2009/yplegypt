79ec06d2555c2dfd34c6401037a11c6a7d462cc4
mazenDev <mazenfathy841@gmail.com>
2026-09-05 16:33:36 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Wraper from "./wraper";
+import HomePage from "./pages/homepage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/" element={<Wraper />}>
+          <Route index element={<Navigate to="/homepage" />} />
+          <Route path="homepage" element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
