de03795747f8e9c8b276eae7bd664247ba059938
mazenDev <mazenfathy841@gmail.com>
2026-09-07 15:45:58 +0300

    adding the about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Wraper from "./wraper";
 import HomePage from "./pages/homepage";
 import "./styles/index.css";
+import About from "./pages/about";
 export default function App() {
   return (
     <BrowserRouter>
@@ -9,7 +10,7 @@ export default function App() {
         <Route path="/" element={<Wraper />}>
           <Route index element={<Navigate to="/about-us" />} />
           <Route path="homepage" element={<HomePage />} />
-          <Route path="about-us" element={<HomePage />} />
+          <Route path="about-us" element={<About />} />
           <Route path="dashboard" element={<HomePage />} />
         </Route>
       </Routes>
